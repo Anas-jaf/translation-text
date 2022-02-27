f6a45a6ea0f7bea8b66a66ffde11d7e16608c82b
Anas-jaf <ansas4565@gmail.com>
2022-02-27 13:35:31 +0300

    first commit

---
+++ asd.py
@@ -0,0 +1,6 @@
+import os 
+
+
+
+https://media.githubusercontent.com/media/Anas-jaf/alnadi_project/master/%D9%86%D8%B5%20%D9%85%D8%B4%D8%B1%D9%88%D8%B9%20%D8%A7%D9%84%D8%AA%D8%B1%D8%AC%D9%85%D8%A9/target/10_%D8%A7%D9%84%D8%A7%D8%B1%D9%82%D8%A7%D9%85/250.txt
+https://media.githubusercontent.com/media/Anas-jaf/alnadi_project/master/%D9%86%D8%B5%20%D9%85%D8%B4%D8%B1%D9%88%D8%B9%20%D8%A7%D9%84%D8%AA%D8%B1%D8%AC%D9%85%D8%A9/target/10_%D8%A7%D9%84%D8%A7%D8%B1%D9%82%D8%A7%D9%85/249.txt
